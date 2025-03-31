108337f4eb60472723eea53701e94a1bffce470a
tumi267 <itu.matlala4@gmail.com>
2025-03-31 04:29:53 +0200

    Fix deployment issues

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images:{
-        domains:['images.unsplash.com','plus.unsplash.com','unsplash.com','media.istockphoto.com',]
+    /** @type {import('next').NextConfig} */
+    const nextConfig = {
+        images:{
+            domains:['images.unsplash.com','plus.unsplash.com','unsplash.com','media.istockphoto.com',]
+        }
+        
     }
-    
-}
 
-module.exports = nextConfig
+    module.exports = nextConfig
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+export const dynamic = 'force-dynamic';
 function notfound() {
   return (
     <div>
